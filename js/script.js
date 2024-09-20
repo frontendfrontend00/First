aec7f1df6bd6e1419c86a2fd7abd7694f705a5dc
Artem <frontendfrontend00@gmail.com>
2024-09-20 21:26:24 +0300

    FIrst JS

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+console.log("arr" + " - obj");
+console.log(4 + +"Hello world");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+//++incr
+//--decr
+incr++;
+// -- і ++ перед значенням префіксна форма
+// -- і ++ після значенням постфіксна форма
+// постфіксна форма повертає спочатку старе значення
+
+console.log(5 % 2);
+
+console.log(2 + 4 === 8);
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+
+// && - і
+// || - або
+// !  - заперечення
+
+console.log(2 + 2 + 2 != "6");
+console.log(2 + 2 + 2 !== "6");
